468ebb8de037af357b5c7d76329736ef46d5005a
minho0315 <dlalsgh369@naver.com>
2021-03-24 23:30:23 +0900

    add myLogger.js

---
+++ src/middlewares/myLogger.js
@@ -0,0 +1,11 @@
+const myLogger = store => next => action => {
+  console.log(action); // 먼저 액션을 출력한다.
+  const result = next(action); // 다음 미들웨어 (또는 리듀서) 에게 액션을 전달
+  
+  // 업데이트 이후의 상태를 조회
+  console.log('\t', store.getState()); // '\t' 는 택 문자 이다.
+  
+  return result; // 여기서 반환하는 값은 dispatch(action)의 결과물이 된다. 기본 : undefined
+}
+
+export default myLogger;
